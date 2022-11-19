3ca4ae1cad316fd0a9f193cf3218cf7f566bc287
VladCan <vladymir.canchari@gmail.com>
2022-11-19 11:18:21 -0500

    Creacion del Proyecto Harry Potter

---
+++ Harry_Potter/HarryPotter/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: VLADYMIR
+ *
+ * Created on 19 de noviembre de 2022, 11:16
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
